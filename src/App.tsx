83deef3fe787f2e4caa679446dd02fd193cf8cb0
Aradhya Raj <aradhyaraj7422@gmail.com>
2026-08-11 22:29:03 +0530

    Auto Update - 11/8/2026, 10:29:03 pm

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,6 @@ function App() {
       <CustomCursor />
       <ScrollProgress />
       <Navbar />
-      
-      {/* Global Animated Background Orbs */}
-      <div className="fixed inset-0 overflow-hidden pointer-events-none -z-20">
-        <div className="absolute top-0 -left-4 w-72 h-72 bg-primary rounded-full mix-blend-multiply filter blur-[128px] opacity-70 animate-blob"></div>
-        <div className="absolute top-0 -right-4 w-72 h-72 bg-secondary rounded-full mix-blend-multiply filter blur-[128px] opacity-70 animate-blob animation-delay-2000"></div>
-        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-accent rounded-full mix-blend-multiply filter blur-[128px] opacity-70 animate-blob animation-delay-4000"></div>
-      </div>
 
       <main>
         <Hero />
